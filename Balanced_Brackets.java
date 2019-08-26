be08c7250f1c719ee42cb135a6928eafbbee4370
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-08-26 15:30:58 +0530

    Final correct working code committed.

--- Balanced_Brackets.java
+++ Balanced_Brackets.java
@@ -1,35 +1,39 @@
 import java.io.*;
 import java.util.*;
 
-public class Balanced_Brackets{
-static class Stack{
-int top=-1;
-char items[]=new char[100];
+public class Balanced_Brackets
+{
+    static class Stack
+    {
+    int top=-1;
+    char items[]=new char[100];
+    void push(char x)
+        {
+        if(top==99)
+            {  
+            System.out.println("Stack is full");
+            }   
+        else
+        items[++top]=x;
+        } 
 
-void push(char x){//2
-if(top==99){  //1
-System.out.println("Stack is full");
-}   //1
+char pop(){
+if(top==-1){ 
+System.out.println("Stack is empty");
+return '\0';
+} 
 else
-items[++top]=x;
-} //2
-
-char pop(){//5
-if(top==-1){ //3
-System.ou.println("Stack is empty");
-} //3
-else
-{ //4
+{ 
 char element = items[top];
 top--;
-retrun element;
-} //4
-}//5
+return element;
+} 
+}
 
-boolean isEmpty(){//6
+boolean isEmpty(){
 return (top==-1)?true:false;
-} //6
-
+} 
+}
 static boolean isMatchingPair(char c1, char c2){
 if(c1=='(' && c2==')'){
 return true;}
@@ -41,7 +45,8 @@ else
 return false;
 }
 
-static boolean Balanced(exp[]){
+static boolean Balanced(char exp[])
+{
 Stack st = new Stack();
 for(int i=0;i<exp.length;i++){
 if(exp[i]=='{' || exp[i]=='(' || exp[i]=='['){
@@ -64,17 +69,11 @@ return false;
 }
 public static void main(String[] args)  
     { 
-        char exp[] = {'{','(',')','}','[',']'}; 
+        char exp[] = {'{','(',')','}','[',']', '{'}; 
           if (Balanced(exp)) 
             System.out.println("Balanced "); 
           else
             System.out.println("Not Balanced ");   
     } 
-  
-}   
- 
-
-
-
-
-		 
+    
+}
