74cc2d1036179fd000bda33ec7eddabe087e9f39
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-09-03 20:31:46 +0530

    First commit for infix to postfix.

---
+++ infixtopostfix.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Postfix
+{
+    static int postfix(String exp)
+    {
+        Stack<Integer> stk = new Stack<>();
+        for(int i=0; i<exp.length(); i++)
+        {
+            char c = exp.charAt(i);
+            if(Character.isDigit(c))
+            {
+                
+            }
+        }
+    }
+}
