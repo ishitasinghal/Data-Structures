771acd95535917431ac6e508d4788ea4ae90bef1
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-09-03 20:42:13 +0530

    Final commit.

--- infixtopostfix.java
+++ infixtopostfix.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class Postfix
+public class Postfix
 {
     static int postfix(String exp)
     {
@@ -9,8 +9,37 @@ class Postfix
             char c = exp.charAt(i);
             if(Character.isDigit(c))
             {
+                stk.push(c-'0');  //this converts the character to integer
+            }
+            else
+            {
+                int v1 = stk.pop();
+                int v2 = stk.pop();
                 
+                switch(c)
+                {
+                    case '+':
+                        stk.push(v1+v2);
+                        break;
+                    case '-':
+                        stk.push(v2-v1);
+                        break;
+                    case '*':
+                        stk.push(v2*v1);
+                        break;
+                    case '/':
+                        stk.push(v2/v1);
+                        break;
+                }
             }
         }
+        
+        return stk.pop();
+    }
+    
+    public static void main(String args[])
+    {
+        String exp="231*+9-"; 
+        System.out.println("postfix evaluation: "+postfix(exp)); 
     }
 }
