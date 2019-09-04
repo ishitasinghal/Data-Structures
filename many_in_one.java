28f67f139a6459963f7d73952a7586e28db72d33
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-09-04 20:04:28 +0530

    Initial commit.

---
+++ many_in_one.java
@@ -0,0 +1,10 @@
+//program to implement k stacks from one array.
+public class Manystack
+{
+    static class Kstack
+    {
+        int arr[]; //actual data to b stored in stacks
+        int top[]; //to store indexes of top element
+        int next[];
+    }
+}
