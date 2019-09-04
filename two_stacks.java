9bda419f2081751d252e085698c06bb8278af30d
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-09-04 19:14:28 +0530

    Commit 1 .

---
+++ two_stacks.java
@@ -0,0 +1,9 @@
+//this program is to implement two stacks using a sinlge array.
+//for this, we r using a space efficient approach by pushing the elements from the extreme corners of the array, which would utilize the array space efficiently.
+
+class Twostacks
+{
+    int size;
+    int top1, top2;
+    int arr[];
+}
