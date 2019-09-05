41b7bbcf94d3abbc7e9f31f42edfd76b3fe8265c
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-09-05 16:20:19 +0530

    Commit 2 for the code.

--- many_in_one.java
+++ many_in_one.java
@@ -5,6 +5,25 @@ public class Manystack
     {
         int arr[]; //actual data to b stored in stacks
         int top[]; //to store indexes of top element
-        int next[];
+        int next[];  //next entry of all stacks
+        int n,k;
+        int free; //beginning index
+        
+        Kstack(int k1, int n1)
+        {
+            k=k1;
+            n=n1;
+            arr new int[n];
+            top=new int[k];
+            next=new int[n];
+            
+            for(int i=0;i<k;i++)   //initializing all stacks as empty
+             top[i]=-1;
+             
+             free=0;
+             for(int i=0;i<n;i++)
+             next[i]=i+1;
+             next[n-1]=-1;
+        }
     }
 }
