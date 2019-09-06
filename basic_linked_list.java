ca45ad4377212211da937a6ffae1f1ae69037354
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-09-06 15:48:25 +0530

    Done!

--- basic_linked_list.java
+++ basic_linked_list.java
@@ -37,3 +37,50 @@ public class Linkedlist
         ls.print();
     }
 }
+
+
+//practice
+
+public class Linkedlist
+{
+    Node head;
+    
+    static class Node
+    {
+        int data;
+        Node next;
+        
+        Node(int d)
+        {
+            data=d;
+            next=null;
+        }
+    }
+    
+    void print()
+    {
+        Node n =head;
+        while(n!=null)
+        {
+            System.out.print(n.data+" ");
+            n= n.next;
+        }
+    }
+    
+    public static void main(String args[])
+    {
+        Linkedlist ls= new Linkedlist();
+        ls.head=new Node(1);
+        Node one = new Node(23);
+        Node two = new Node(87);
+        Node three = new Node(76);
+        Node four = new Node(100);
+        
+        ls.head.next=one;
+        one.next=two;
+        two.next=three;
+        three.next=four;
+        
+        ls.print();
+    }
+}
