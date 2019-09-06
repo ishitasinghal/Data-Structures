b7d4257533f63dfc00b4dcdb96ed3f0a303deaaf
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-09-06 15:29:41 +0530

    commit 1
    This is just a basic program that simply creates three nodes in a linked list.

---
+++ basic_linked_list.java
@@ -0,0 +1,28 @@
+//basic linkeed-list program
+public class Linkedlist
+{
+    Node head; //head of type node
+    static class Node
+    {
+        int data;
+        Node next;  //next is by default initialized by null
+        Node(int d)  //parameterized constructor to initialize vlaues of Node class data members
+        {
+            data=d;
+            next=null;
+        }
+    }
+    
+    public static void main(String args[])
+    {
+        Linkedlist ls=new Linkedlist();
+        ls.head=new Node(1);
+        Node two = new Node(2);
+        Node three = new Node(3);
+        
+        //here basically the three nodes have been made in which each points to null ls.head is the first one
+        ls.head.next = two;
+        two.next=three;  //next pointer of the second node now points to the third node
+        
+    }
+}
