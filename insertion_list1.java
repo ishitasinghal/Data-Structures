02032fbcee2b45245238be022b6744392d9a747a
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-09-07 17:26:16 +0530

    Done!

---
+++ insertion_list1.java
@@ -0,0 +1,56 @@
+//inserting inside a linked list
+public class Insertionatfront
+{
+    Node head;
+    
+    static class Node
+    {
+        int data;
+        Node next;
+        
+        Node(int d)
+        {
+            data=d;
+            next=null;
+        }
+    }
+    
+    //inserting a node at front of the list
+    public void push(int element)
+    {
+        Node new_node = new Node(element);   //making the new node
+        new_node.next=head;          //next becomes the head
+        head=new_node;   //now head points to the new node
+    }
+    
+    public void print()
+    {
+        Node n=head;
+        while(n!=null)
+        {
+            System.out.print(n.data+" ");
+            n=n.next;
+        }
+    }
+    
+    public static void main(String args[])
+    {
+        Insertionatfront iaf = new Insertionatfront();
+        iaf.head = new Node(1);
+        
+        Node one = new Node(2);
+        Node two = new Node(3);
+        Node three = new Node(4);
+        Node four = new Node(5);
+        
+        iaf.head.next=one;
+        one.next=two;
+        two.next=three;
+        three.next=four;
+        
+        iaf.print();
+        iaf.push(100);
+        iaf.print();
+        
+    }
+}
