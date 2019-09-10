68d8482cd1577d441f8f7baef2c42c1e476d5064
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-09-10 22:09:53 +0530

    Commit 2.

--- list_deletion1.java
+++ list_deletion1.java
@@ -1,9 +1,68 @@
 //deleting a key from a singly linked list
+import java.io.*;
 public class Dellist
 {
     Node head;
-    class Node
+    static class Node
     {
+        int data;
+        Node next;
+        
+        Node(int d)
+        {
+            data=d;
+            next=null;
+        }
+    }
+    
+    //when a key is provided, this func deletes the first occurence of the key
+    public void firstdel(int key)
+    {
+        Node temp=head, prev=null; //head node stored
+        //if the first node is the key itself
+        if(temp!=null && temp.data==key)
+        {
+            head=temp.next;  //head changed
+            return;
+        }
+        
+        while(temp!=null && temp.data!=key)
+        {
+            prev=temp;
+            temp=temp.next;
+        }
+        
+        //if key is absent
+        if(temp==null)
+        return;
+        
+        prev.next=temp.next;
+    }
+    
+    public void push(int element)
+    {
+        Node n = new Node(element);
+        n.next=head;
+        head=n;
+    }
+    
+    public void print()
+    {
+        Node no=head;
+        while(no!=null)
+        {
+            System.out.print(no.data+" ");
+            n=n.next;
+        }
+    }
+    
+    public static void main(String args[])throws IOException
+    {
+        Dellist dl=new Dellist();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("How many elements u wanna put into the list?");
+        int n = Integer.parseInt(br.readLine());
+        
         
     }
 }
