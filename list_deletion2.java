07fa6615fc6ab6b22c1f90d4a2c6d640da3456be
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-09-12 16:24:09 +0530

    Commit 2.

--- list_deletion2.java
+++ list_deletion2.java
@@ -42,8 +42,42 @@ public class Posdel
         temp=temp.next;
         
         //if position given is greater than the number of nodes
-        if(temp==nu|| temp.next==null)
+        if(temp==null || temp.next==null)
         return;
+        
+        Node next=temp.next.next;    //storing the pointer of the next node to be deleted
+        temp.next=next;  //free the deleted node from the list   
     }
     
+    public void print()
+    {
+        Node n=head;
+        while(n!=null)
+        {
+            System.out.println(n.data+" ");
+            n=n.next;
+        }
+    }
+}
+
+class Demo
+{
+    public static void main(String args[])
+    {
+        Posdel pd=new Posdel();
+        pd.insert(10);
+        pd.insert(20);
+        pd.insert(30);
+        pd.insert(4);
+        pd.insert(50);
+        pd.insert(60);
+        
+        System.out.println("Initially created list is ! ");
+        pd.print();
+        
+        pd.delkey(3);
+        System.out.println("Now the list is ! ");
+        pd.print();
+        
+    }
 }
