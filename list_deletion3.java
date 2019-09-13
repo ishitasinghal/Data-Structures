b6ad5acde762a7941f958c5fae75220eebbaa0ab
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-09-13 15:51:18 +0530

    Initial commit!

---
+++ list_deletion3.java
@@ -0,0 +1,15 @@
+//program to complete the whole list
+public class Deletelist
+{
+    Node head;
+    static class Node
+    {
+        Node next;
+        int data;
+        Node(int d)
+        {
+            data=d;
+            next=null;
+        }
+    }
+}
