832fd75d851d9436f89c4ce631d497ca703d0882
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-09-14 09:21:21 +0530

    Commit 2.

--- list_deletion3.java
+++ list_deletion3.java
@@ -12,4 +12,41 @@ public class Deletelist
             next=null;
         }
     }
+    
+    //function to delete the whole list
+    public void delete()
+    {
+        head = null; //garbage collection happens on its own in java
+    }
+    
+    public void insert(int element)
+    {
+        Node n = new Node(element);
+        n.next=head;
+        head=n;
+    }
+    
+    public void print()
+    {
+        Node no;
+        while(no!=null)
+        {
+            System.out.print(no.data+" ");
+            no=no.next;
+        }
+    }
+    
+    public static void main(String args[])
+    {
+        Deletelist dl = new Deletelist();
+        dl.insert(10);
+        dl.insert(20);
+        dl.insert(30);
+        dl.insert(94);
+        System.out.print("Initially the list is : ");
+        dl.print();
+        dl.delete();
+        System.out.print("Deleted list is: ");
+        dl.print();
+    }
 }
