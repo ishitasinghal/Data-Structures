0316bef37a819ecb19bc3e55e4e380c6fa3f9c8b
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-09-15 10:16:09 +0530

    Initial commit!

---
+++ searching1.java
@@ -0,0 +1,50 @@
+//searching an element in the linked list by iterative approach
+class Node
+{
+    int data; 
+    Node next;
+    Node(int d)
+    {
+        data=d;
+        next=null;
+    }
+}
+
+class Itersearch
+{
+    Node head;
+    public void insert(int element)
+    {
+        Node n = new Node(element);
+        n.next=head;
+        head=n;
+    }
+    
+    public boolean search(Node head, int item)
+    {
+        Node current = head;
+        while(current!=null)
+        {
+            if(current.data==item)
+            {
+                return true;
+            }
+            current=current.next;
+        }
+        return false;
+    }
+    
+    public static void main(String args[])
+    {
+        Itersearch is = new Itersearch();
+        is.insert(98);
+        is.insert(54);
+        is.insert(46);
+        is.insert(64);
+        is.insert(74);
+        is.insert(63);
+        is.insert(9);
+        
+        System.out.println("Data present =  "+ is.search(is.head, 32));
+    }
+}
