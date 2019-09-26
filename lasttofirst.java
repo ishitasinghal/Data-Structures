12f7c27036e8d6c2b6ca374b5025284c8bc52230
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-09-26 22:30:00 +0530

    Committing changes!

--- lasttofirst.java
+++ lasttofirst.java
@@ -1,5 +1,41 @@
 //moving last element to the front of the linked list
 public class Lasttofront
 {
-    
+    Node head;
+    class Node
+    {
+        int data;
+        Node next;
+        Node(int d)
+        {
+            data=d;
+            next=null;
+        }
+    }
+    //function to insert an element into a list
+    void insert(int element)
+    {
+        Node n = new Node(element);
+        n.next=head;
+        head=n;
+    }
+   //function to display the list 
+    void print()
+    {
+        Node h = head;
+        while(h!=null)
+        {
+            System.out.print("h.data"+" ");
+            h=h.next;
+        }
+        System.out.println();
+    }
+    //function to move the last element to the front
+    void frontmove()
+    {
+        if(head==null || head.next==null)
+        return;
+        
+        Node 
+    }
 }
