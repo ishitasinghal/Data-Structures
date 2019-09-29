232362fbedb1112f0039c3155db1d8c5269f0a6e
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-09-29 22:33:34 +0530

    Committing updates!

--- eve_odd.java
+++ eve_odd.java
@@ -20,9 +20,9 @@ public class Eveodd
         Node curr = head;
         
         while(last.next!=nul)
-        end = end.next;
+        last = last.next;
         
-        Node n = end;
+        Node n = last;
         while(curr.data%2 !=0 && curr!=end)
         {
             new_last = curr;
@@ -42,8 +42,59 @@ public class Eveodd
                 else
                 {
                     prev.next = curr.next;
+                    curr.next = null;
+                    new_last.next=curr;
+                    new_last=curr;
+                    curr=prev.next;
                 }
             }
         }
+        else
+        prev=curr;
+        if(new_last!=last && last.data%2!=0)
+        {
+            prev.next=last.next;
+            last.next=null;
+            new_last.next=end;
+        }
+    }
+    void insert(int element)
+    {
+        Node no = new Node(element);
+        no.next=head;
+        head=no;
+    }
+    void print()
+    {
+        Node nod = head;
+        while(nod.next!=null)
+        {
+            System.out.print(nod.data+" ");
+            nod=nod.next;
+        }
+        System.out.println();
+    }
+    
+    public static void main(String args[])
+    {
+        Eveodd eo = new Eveodd();
+        eo.insert(11);
+        eo.insert(99);
+        eo.insert(84);
+        eo.insert(93);
+        eo.insert(72);
+        eo.insert(64);
+        eo.insert(43);
+        eo.insert(2);
+        eo.insert(14);
+        eo.insert(3);
+        
+        System.out.println("Origional Linked List"); 
+        eo.print();
+        
+        System.out.println("Final Linked List");
+        eo.seperator();
+        eo.print(); 
+        
     }
 }
