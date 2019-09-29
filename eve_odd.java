0c108c75a84161cb93621ecaad2bbf9e58fc8573
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-09-29 22:23:43 +0530

    Committing changes!!

--- eve_odd.java
+++ eve_odd.java
@@ -14,4 +14,36 @@ public class Eveodd
     }
     
     public void seperator()
+    {
+        Node last = head;
+        Node prev = null;
+        Node curr = head;
+        
+        while(last.next!=nul)
+        end = end.next;
+        
+        Node n = end;
+        while(curr.data%2 !=0 && curr!=end)
+        {
+            new_last = curr;
+            curr = curr.next;
+            new_last.next.next = null;
+            new_last = new_last.next;
+        }
+        if(curr.data%2==0)
+        {
+            head = curr;
+            while(curr!=end){
+                if(curr.data%2==0)
+                {
+                    prev = curr;
+                    curr = curr.next;
+                }
+                else
+                {
+                    prev.next = curr.next;
+                }
+            }
+        }
+    }
 }
