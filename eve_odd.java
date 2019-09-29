110c5216a404c5ad52dab94c737ee2ea0041363e
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-09-29 22:14:40 +0530

    Initial commit!

---
+++ eve_odd.java
@@ -0,0 +1,17 @@
+//playing eve odd with the list
+public class Eveodd
+{
+    Node head;
+    class Node
+    {
+        int data;
+        Node next;
+        Node(int d)
+        {
+            data=d;
+            next=null;
+        }
+    }
+    
+    public void seperator()
+}
