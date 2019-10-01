7736c26aeab22cc6376c3bdc67512be6d75c52df
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-10-01 19:58:37 +0530

    Initial commit!

---
+++ sentence_reversal.java
@@ -0,0 +1,16 @@
+//sentence reversal
+import java.util.*;
+
+public class Sentrev
+{
+    static void rev(String sen)
+    {
+        Stack<String> s = new Stack<>();
+        String ar[] = sen.split(" ");
+        for(int i=0;i<ar,length;i++)
+        {
+            s.push(ar[i]);
+        }
+        while(!s.empty())
+    }
+}
