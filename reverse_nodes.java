181dd62c3a11f1bcd7643615b505a1c22cc66236
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-10-01 19:39:02 +0530

    Final commit !

--- reverse_nodes.java
+++ reverse_nodes.java
@@ -29,14 +29,14 @@ public class Revstack
     
     public static void main(String args[])
     {
-        st.push(1);
-        st.push(2);
-        st.push(3);
-        st.push(4);
-        st.push(5);
-        st.push(6);
-        st.push(7);
-        st.push(8);
+        st.push('1');
+        st.push('2');
+        st.push('3');
+        st.push('4');
+        st.push('5');
+        st.push('6');
+        st.push('7');
+        st.push('8');
         
         System.out.println("Initial stack: ");
         System.out.println(st);
