152dd2e8304995671a18224f1c190352b51f66ec
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-10-02 22:58:04 +0530

    updating file!

--- queue1.java
+++ queue1.java
@@ -31,5 +31,16 @@ public class Queueviastack
             return x;
         }
     }
+    public static void main(String args[])
+    {
+        Queue q = new Queue();
+        q.enQueue(1);
+        q.enQueue(2);
+        q.enQueue(3);
+        
+        System.out.println(q.deQueue());
+        System.out.println(q.deQueue());
+        System.out.println(q.deQueue());
+    }
     
 }
