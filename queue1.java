e846f1aebdde575cc55858c6622d87ae2aa690a9
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-10-02 22:53:03 +0530

    Committing changes!

--- queue1.java
+++ queue1.java
@@ -1,3 +1,19 @@
 //implementing queues using stacks
 import java.util.*;
-public class Queue
+public class Queueviastack
+{
+    static class Queue
+    {
+        static Stack<Integer> s1 = new Stack<Integer>();
+        static Stack<Integer> s2 = new Stack<Integer>();
+        
+        static void enQueue(int x)
+        {
+            while(!s1.isEmpty())
+            {
+                s2.push(s1.pop());
+            }
+            s1.push(x);
+        }
+    }
+}
