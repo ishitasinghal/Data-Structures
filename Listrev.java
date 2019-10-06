dde52c8fb2f516c4e84537f1eb210b49c74cdef4
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-10-06 16:47:51 +0530

    First commit !

---
+++ Listrev.java
@@ -0,0 +1,19 @@
+//reversing a linked list 
+public class Listrev
+{
+    static Node head;
+    static class Node
+    {
+        int data;
+        Node next;
+        NOde(int d)
+        {
+            next=null;
+            data=d;
+        }
+    }
+    Node rev(Node node)
+    {
+        
+    }
+}
