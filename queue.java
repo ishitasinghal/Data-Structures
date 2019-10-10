dd6af6b950a0fa3200caf81a6f10d3f56c52c6b4
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-10-10 12:08:54 +0530

    Errors needs to be resolved !

--- queue.java
+++ queue.java
@@ -1,94 +1,92 @@
-// implementing queue by array
-public class Queue
+//implementing queue by array
+class Queue
 {
-    int front, rear, size;
-    int length;
-    int array;
-    
-    Queue(int length)
-    {
-        this.length = length;
-        front = this.size=0;
-        rear= length-1;
-        array = new int[this.capacity];
-    }
-    
-    //checking if Queue doesn't have more space left.
-    boolean isfull(Queue q)
-    {
-        return(q.size==q.length);
-    }
-    
-    //checking if the queue is Empty.
-    boolean isempty(Queue q)
-    {
-        return(q.size==0);
-    }
-    
-    //enqueue function to insert the elemnrs in the queue
-    void enqueue(int element)
-    {
-        if(isfull(this))
-        {
-            return;
-            this.rear = (this.rear +1)%this.length;
-            this.array[this.rear] = element;
-            this.size = this.size+1;
-            System.out.println(element+"Inserted into the queue");
-        }
-    }
-    
-    //dequeue function queue to delete an element
-    int dequeue()
-    {
-        if(isempty(this))
-        return Integer.MIN_VALUE;
-        int item = this.array[this.front];
-        this.front = (this.front+1)%this.length;
-        this.size = this.size=1;
-        return item;
-    }
-    
-    //getting the front of the queue
-    void front()
-    {
-        if(isempty(this))
-        {
-            return Integer.MIN_VALUE;
-        }
-        return this.array[this.front];
-    }
-    
-    //getting the rear of the queue
-    void rear()
-    {
-        if(isempty(this))
-        {
-            return Integer.MIN_VALUE;
-        }
-        return this.array[this.rear];
-    }
+ int front, rear, size;
+ int length;
+ int array;
+ 
+ Queue(int length)
+ {
+     this.length = length;
+     front = this.size=0;
+     rear= length-1;
+     array = new int[this.length];
+ }
+ 
+ //checking if Queue doesn't have more space left.
+ boolean isfull(Queue q)
+ {
+     return(q.size==q.length);
+ }
+ 
+ //checking if the queue is Empty.
+ boolean isempty(Queue q)
+ {
+     return(q.size==0);
+ }
+ 
+ //enqueue function to insert the elemnrs in the queue
+ void enqueue(int element)
+ {
+     if(isfull(this))
+    	 return;
+     this.rear = (this.rear + 1)%this.length; 
+     this.array[this.rear] = element; 
+     this.size = this.size + 1; 
+     System.out.println(element+ " enqueued to queue"); 
+ }
+ 
+ //dequeue function queue to delete an element
+ int dequeue()
+ {
+     if(isempty(this))
+     return Integer.MIN_VALUE;
+     int element = this.array[this.front];
+     this.front = (this.front+1)%this.length;
+     this.size = this.size=1;
+     return element;
+ }
+ 
+ //getting the front of the queue
+ int front()
+ {
+     if(isempty(this))
+     {
+         return Integer.MIN_VALUE;
+     }
+     return this.array[this.front];
+ }
+ 
+ //getting the rear of the queue
+ int rear()
+ {
+     if(isempty(this))
+     {
+         return Integer.MIN_VALUE;
+     }
+     return this.array[this.rear];
+ }
 }
 public class Mainc
 {
-    public static void main(Stirng args[])
-    {
-        Queue q = new Queue(500);
-        q.enqueue(1);
-        q.enqueue(2);
-        q.enqueue(3);
-        q.enqueue(4);
-        q.enqueue(5);
-        q.enqueue(6);
-        q.enqueue(7);
-        q.enqueue(8);
-        q.enqueue(9);
-        
-        System.out.println("## BEFORE ##");
-        System.out.println("Front element"+q.front());
-        System.out.println("Rear element"+q.rear());
-        System.out.println("## AFTER ##"");
-        System.out.println("Front element"+q.front());
-        System.out.println("Rear element"+q.rear());
-    }
+ public static void main(String args[])
+ {
+     Queue q = new Queue(500);
+     q.enqueue(1);
+     q.enqueue(2);
+     q.enqueue(3);
+     q.enqueue(4);
+     q.enqueue(5);
+     q.enqueue(6);
+     q.enqueue(7);
+     q.enqueue(8);
+     q.enqueue(9);
+     
+     System.out.println("## BEFORE ##");
+     System.out.println( q.front()+ "Front element");
+     System.out.println("Rear element"+q.rear());
+     System.out.println("## AFTER ##");
+     System.out.println("Front element"+q.front());
+     System.out.println("Rear element"+q.rear());
+ }
 }
