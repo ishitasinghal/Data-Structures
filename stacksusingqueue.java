96437fe72a3dd01d18190ba5e1e8a3f442f387de
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-10-13 20:39:45 +0530

    Commit 1 !
    Implementing stacks using queue. !

---
+++ stacksusingqueue.java
@@ -0,0 +1,12 @@
+//implementing stacks using queues
+import java.util.LinkedList;
+import java.util.Queue;
+class Squeue
+{
+	Queue<Integer> q = new LinkedList<Integer>();
+	//inserting in the queue
+	void push(int item)
+	{
+		
+	}
+}
