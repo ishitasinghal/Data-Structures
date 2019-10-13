37eb6cc27cbea773d158eca5d6a4e95d4b7ed14c
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-10-13 20:45:20 +0530

    Adding push and pop functions !

--- stacksusingqueue.java
+++ stacksusingqueue.java
@@ -7,6 +7,26 @@ class Squeue
 	//inserting in the queue
 	void push(int item)
 	{
-		
+		int size=q.size();
+		q.add(item);
+		for(int i=0;i<size;i++)
+		{
+			int x=q.remove();
+			q.add(item);
+		}
 	}
+	
+	//removing the top element 
+	int pop()
+	{
+		if(q.isEmpty())
+		{
+			System.out.print("No elements");
+			return -1;
+		}
+		int x=q.remove();
+		return x;
+	}
+	
+	
 }
