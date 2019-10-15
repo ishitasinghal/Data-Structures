5a4bf13815df314d288db77406dd2a83d877b91c
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-10-16 00:08:55 +0530

    Function 1 committed !

--- queue2.java
+++ queue2.java
@@ -1,4 +1,4 @@
-//finding reverse of queue
+//finding reverse of queue for first given elements
 import java.util.LinkedList;
 import java.util.Stack;
 import java.util.Queue;
@@ -6,3 +6,16 @@ import java.util.Queue;
 public class Reverse
 {
 static Queue<Integer> q;
+static void reversefirstkele(int k)
+{
+  if(q.isEmpty()==true || k>q.size())
+    return;
+  if(k<=0)
+    return;
+  Stack<Integer> s = new Stack<Integer>();
+  //pushing elements into the stack
+  for(int i=0;i<k;i++)
+    s.push(q.peek());
+  q.remove();
+}
+  
