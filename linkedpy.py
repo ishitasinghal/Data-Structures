01bbbef5dd6a0f752f8fd225c071299653621c74
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2020-01-13 18:00:49 +0530

    Update linkedpy.py

--- linkedpy.py
+++ linkedpy.py
@@ -2,3 +2,12 @@ class Node:
     def __init__(self, data=None, next_node=None):
         self.data=data
         self.next_node = next_node
+        
+    def getdata(self):
+        return self.data
+    
+    def getnext(self):
+        return self.next_node
+        
+    def setnext(self, new_node):
+        self.nextnode = new_node
