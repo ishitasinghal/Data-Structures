2f5854290bd3e3276b0d216a4787bda528584b21
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2020-01-28 09:51:22 +0530

    Update doublylist.py

--- doublylist.py
+++ doublylist.py
@@ -5,6 +5,7 @@ class Node:
         self.next = next
         slef.prev = prev
         self.data = data
+class Double:
     def addatfront(self, info):
         newnode = Node(data = info)
         newnode.next = self.head
@@ -12,4 +13,17 @@ class Node:
         if(self.head is not None):
             self.head.prev = newnode
         self.head = newnode
+    def printinfo(self, node):
+        while(node is not None):
+            print(node.data)
+            last = node
+            node = node.next
+        
+listobj = Double()
+listobj.push(1)
+listobj.push(2)
+listobj.push(3)
+listobj.push(4)
+listobj.printinfo(listobj.head)
+
         
