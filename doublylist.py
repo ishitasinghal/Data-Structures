3f13141c520686065f890ff05d667d7e5587b943
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2020-01-29 15:22:35 +0530

    Update doublylist.py

--- doublylist.py
+++ doublylist.py
@@ -1,17 +1,14 @@
-#the code has been practiced from "https://www.geeksforgeeks.org/doubly-linked-list/" .
-
 class Node:
-    def __init__(self, next=None, prev=None, data=None):
-        self.next = next
-        self.prev = prev
+    def __init__(self, data):
+        self.next = None
+        self.prev = None
         self.data = data
 class Double:
     def __init__(self):
         self.head = None
     def addatfront(self, info):
-        newnode = Node(data = info)
+        newnode = Node(info)
         newnode.next = self.head
-        newnode.prev = None
         if(self.head is not None):
             self.head.prev = newnode
         self.head = newnode
@@ -24,7 +21,7 @@ class Double:
         if prevnode is None:
             print("NO SUCH NODE EXISTS")
             return
-        newnode = Node(data = info)
+        newnode = Node(info)
         newnode.next = prevnode.next
         prevnode.next = newnode
         newnode.prev = prevnode
@@ -36,5 +33,6 @@ listobj = Double()
 listobj.addatfront(1)
 listobj.addatfront(2)
 listobj.addatfront(3)
-listobj.addatfront(4)
+listobj.addatfront(5)
+listobj.afternode(listobj.head.next,8)
 listobj.printinfo(listobj.head)
