d4f8b961b737912b85705a33e03250950216278f
Ishita Singhal <ishitasinghal08@gmail.com>
2020-10-09 09:40:07 +0530

    working code

--- eve_odd_sort.py
+++ eve_odd_sort.py
@@ -2,15 +2,19 @@
 #the other side of the array
 #without using a thrrd array
 
+def evesort(arr, i, j):
+    while(i<j):
+        while(arr[i]%2==0 and i<j):
+            i+=1
+        while(arr[j]%2==1 and i<j):
+            j-=1
+        if(i<j):
+            arr[i],arr[j]=arr[j],arr[i]
+            i+=1
+            j-=1
+    return(arr)
+        
 arr = [3,4,6,1,8,34,2,9,90,31]
-n=len(arr)
-j=n-1
 i=0
-while(i!=j+1):
-    if(arr[i]%2==1 and arr[j]%2==0):
-        arr[i],arr[j]=arr[j],arr[i]
-        i+=1
-        j-=1
-    elif(arr[i]%2==1 and arr[j]%2==1):
-        i+=1
-print(arr)
+j=len(arr)-1
+print(evesort(arr,i,j))   
