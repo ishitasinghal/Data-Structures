f1419409be280b753a6349de041fc342b6b2a0ca
Ishita Singhal <ishitasinghal08@gmail.com>
2020-10-16 20:25:49 +0530

    Update remove.py

--- remove.py
+++ remove.py
@@ -1,13 +1,15 @@
-Given nums = [3,2,2,3], val = 3,
+# Given nums = [3,2,2,3], val = 3,
 
-Your function should return length = 2, with the first two elements of nums being 2.
+# Your function should return length = 2, with the first two elements of nums being 2.
 
-It doesn't matter what you leave beyond the returned length.
+# It doesn't matter what you leave beyond the returned length.
 
-CODE
+# CODE
 
 def removeElement(self, nums: List[int], val: int) -> int:
-        for i in nums:
-            if(i==val):
+    N=len(nums)-1
+        while N >=0:
+            if(nums[N]==val):
                 nums.remove(val)
+            N-=1
         return(len(nums))
