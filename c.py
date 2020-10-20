201d58d5c7f08068307bb28329e432293e6759d5
Ishita Singhal <ishitasinghal08@gmail.com>
2020-10-20 15:31:35 +0530

    Update c.py

--- c.py
+++ c.py
@@ -1,5 +1,15 @@
-Given two integers dividend and divisor, divide two integers without using multiplication, division, and mod operator.
+# Given two integers dividend and divisor, 
+# divide two integers without using multiplication, 
+# division, and mod operator.
+# Return the quotient after dividing dividend by divisor.
+# The integer division should truncate toward zero, which 
+# means losing its fractional part. For example, 
+# truncate(8.345) = 8 and truncate(-2.7335) = -2.
 
-Return the quotient after dividing dividend by divisor.
-
-The integer division should truncate toward zero, which means losing its fractional part. For example, truncate(8.345) = 8 and truncate(-2.7335) = -2.
+# CODE
+def divide(divisor, dividend):
+    res=0
+    if(divisor<0):
+        res=dividend//(-1*divisor)
+        return(-1*res)
+    return(dividend//divisor)
