584e1f21e3dfa088d7506e4529c021cc9a778f86
Ishita Singhal <ishitasinghal08@gmail.com>
2020-10-20 18:46:59 +0530

    Update c.py

--- c.py
+++ c.py
@@ -7,9 +7,25 @@
 # truncate(8.345) = 8 and truncate(-2.7335) = -2.
 
 # CODE
-def divide(divisor, dividend):
-    res=0
-    if(divisor<0):
-        res=dividend//(-1*divisor)
-        return(-1*res)
-    return(dividend//divisor)
+flag = (dividend < 0) ^ (divisor < 0)
+        u = abs(dividend)
+        d = abs(divisor)
+        
+        m = 1
+        nd = d
+        while nd<<1 <= 1<<31:
+            nd <<= 1
+            m <<= 1
+        
+        ans = 0
+        while nd >= d:
+            if u >= nd:
+                ans += m
+                u -= nd
+            m >>= 1
+            nd >>= 1
+        
+        if flag:
+            return max(-1<<31, min((1<<31)-1, -ans))
+        else:
+            return max(-1<<31, min((1<<31)-1, ans))
